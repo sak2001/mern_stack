b8261ba13e35f48201df4a723dbd7982ada50bb8
sak2001 <sakshamsingh2001@gmail.com>
2021-09-25 13:06:55 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json({limit: "30mb", extended : true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended : true}));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://saksham_project:mongodbislub@cluster0.nmgve.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://saksham_project:<password>@cluster0.nmgve.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
